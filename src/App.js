b163d32c85c280536a1e2cfea332eb04e3b96551
unknown <ziaul935@gmail.com>
2020-11-20 16:34:38 +0600

    service component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Services from './components/TopBanner/Services';
+import TopBanner from "./components/TopBanner/TopBanner";
+
+
+function App() {
+  return (
+    <div>
+<TopBanner/>
+<Services/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TopBanner/Services.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {Container, Col, Row} from 'react-bootstrap'
+
+class Services extends Component {
+    render() {
+        return (
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                        <h1>col1</h1>
+                        </Col>
+         
+                        <Col>
+                        <h1>col1</h1>
+                        </Col>
+                        <Col>
+                        <h1>col1</h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+}
+
+export default Services;
